59cf1cf52a62260a0fcd7f841e4ba4e64a8d8eb3
Valerio <valerio@bruttocarattere.org>
2020-12-10 10:55:41 +0100

    day 10

---
+++ python/10.py
@@ -0,0 +1,38 @@
+import pathlib
+import os
+
+path = pathlib.Path('.').parent / "inputs/10.txt"
+with open(path, "r") as f:
+    lines = f.read().splitlines()
+
+list_numbers = sorted([int(x) for x in lines])
+
+
+## p1
+sum_diff_1 = 1
+sum_diff_3 = 1
+
+for i in range(len(list_numbers)-1):
+    a = list_numbers[i+1]
+    b = list_numbers[i]
+    if a - b == 1:
+        sum_diff_1 += 1
+    elif a - b == 3:
+        sum_diff_3 += 1
+    else:
+        pass
+
+print(sum_diff_1 * sum_diff_3)
+
+## p2
+list_of_combination = [0] * len(list_numbers)
+enumeration = list(enumerate(list_numbers))
+for i, x in enumeration:
+    actual_adapter = x
+    candidates = [j for j, x in enumeration if actual_adapter - 3 <= x <= actual_adapter-1]
+    list_of_combination[i] = sum([list_of_combination[j] for j in candidates])
+    if 0 <= actual_adapter <= 3:
+        list_of_combination[i] += 1
+    pass
+
+print(list_of_combination[-1])
