810c3f0650552a29817cb8e608356896f2ad3d64
Valerio <valerio.figliuolo@contentsquare.com>
2022-12-08 16:25:10 +0100

    a star is a star

--- 2022/src/main/scala/dev/scartiloffista/Seven.scala
+++ 2022/src/main/scala/dev/scartiloffista/Seven.scala
@@ -4,30 +4,32 @@ import utils.ReadFile
 
 case class File(name: String, size: Int)
 
-case class Node(name: String, var folders: Seq[Node], var files: Seq[File], parent: Option[Node], var size: Int = 0){
-  override def toString() = s"Node($name)"
+case class Node(
+    name: String,
+    var folders: Seq[Node],
+    var files: Seq[File],
+    parent: Option[Node],
+    var size: Int = 0
+) {
+  override def toString = s"Node($name)"
 }
 
 object Seven extends App {
 
-  var targets: List[Node] = List[Node]()
+  var nodes: List[Node] = List[Node]()
 
   def navigate(node: Node): Int = {
-//    println(s"visiting ${node.name}")
 
     var acc = 0
-    for(child <- node.folders){
+    for (child <- node.folders) {
       acc += navigate(child)
     }
 
     acc += node.files.map(_.size).sum
-    if(acc <= 100000)
-      targets = targets :+ node
-
+    nodes = nodes :+ node
     node.size = acc
     acc
   }
-
 
   val root = Node("/", Nil, Nil, None)
   var current = root
@@ -35,27 +37,38 @@ object Seven extends App {
   val input = ReadFile.getString("7")
   val sections = input.split("\n\\$").toList.map(_.split("\n").map(_.trim)).tail
 
-  for(item <- sections){
+  for (item <- sections) {
     val command = item.head.split(" ")
     command.head match {
-      case "ls" => {
-        val files = item.tail.filter(_.split(" ").head != "dir").map(_.split(" ")).map(x => File(x(1), x(0).toInt))
-        val folders = item.tail.filter(_.split(" ").head == "dir").map(x => Node(x.split(" ")(1), Nil, Nil, Some(current)))
+      case "ls" =>
+        val files = item.tail
+          .filter(_.split(" ").head != "dir")
+          .map(_.split(" "))
+          .map(x => File(x(1), x(0).toInt))
+        val folders = item.tail
+          .filter(_.split(" ").head == "dir")
+          .map(x => Node(x.split(" ")(1), Nil, Nil, Some(current)))
 
         current.files = files
         current.folders = folders
-      }
-      case "cd" => {
+      case "cd" =>
         val folder = command(1)
         folder match {
           case ".." => current = current.parent.get
-          case _ => current = current.folders.find(_.name == folder).get
+          case _    => current = current.folders.find(_.name == folder).get
         }
-      }
     }
   }
 
   navigate(root)
 
-  println(targets.map(_.size).sum)
+  // p1
+  println(nodes.filter(_.size <= 100000).map(_.size).sum)
+
+  // p2
+  val occupiedSpace = root.size
+  val currentFreeSpace = 70000000 - occupiedSpace
+  val neededSpace = 30000000 - currentFreeSpace
+  println(nodes.filter(_.size >= neededSpace).minBy(_.size).size)
+
 }
